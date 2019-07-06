e1e0e104707be4c169eef7d69a9e890e8fdf5d3b
florante <49671861+florthe3rd@users.noreply.github.com>
2019-07-06 22:21:25 +0800

    include cap1

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	// icon animation
+	$('.toggle').on("click", function(){
+			$('.toggle').toggleClass('active');
+		});
+
+	$('.nav-link').on("click", function(){
+		
+	});
+
+
+	
+
+});
